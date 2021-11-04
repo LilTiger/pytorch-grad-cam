7544a84f6ea4f49fd2cade76c15ed0191b6ba51b
Lil Tiger <anjm888@126.com>
2021-11-04 22:54:04 +0800

    personal usage update.

--- cam.py
+++ cam.py
@@ -1,7 +1,10 @@
 import argparse
+import os.path
+
 import cv2
 import numpy as np
 import torch
+import glob
 from torchvision import models
 from pytorch_grad_cam import GradCAM, \
     ScoreCAM, \
@@ -88,47 +91,56 @@ if __name__ == '__main__':
     # find_layer_types_recursive(model, [torch.nn.ReLU])
     target_layers = [model.layer4[-1]]
 
-    rgb_img = cv2.imread(args.image_path, 1)[:, :, ::-1]
-    rgb_img = np.float32(rgb_img) / 255
-    input_tensor = preprocess_image(rgb_img,
-                                    mean=[0.485, 0.456, 0.406],
-                                    std=[0.229, 0.224, 0.225])
+    # 此处读入一个文件夹中所有图片 并标记热力图
+    # 将文件夹中的所有文件用glob读入 并存入一个列表image_list
+    # 故 --image-path 的输入路径 格式为 './insects/*.png'
+    image_list = glob.glob(''.join(args.image_path))
+    for image in image_list:
+        rgb_img = cv2.imread(image, 1)[:, :, ::-1]
+        rgb_img = np.float32(rgb_img) / 255
+        input_tensor = preprocess_image(rgb_img,
+                                        mean=[0.485, 0.456, 0.406],
+                                        std=[0.229, 0.224, 0.225])
 
-    # If None, returns the map for the highest scoring category.
-    # Otherwise, targets the requested category.
-    target_category = None
+        # If None, returns the map for the highest scoring category.
+        # Otherwise, targets the requested category.
+        target_category = None
 
-    # Using the with statement ensures the context is freed, and you can
-    # recreate different CAM objects in a loop.
-    cam_algorithm = methods[args.method]
-    with cam_algorithm(model=model,
-                       target_layers=target_layers,
-                       use_cuda=args.use_cuda) as cam:
+        # Using the with statement ensures the context is freed, and you can
+        # recreate different CAM objects in a loop.
+        cam_algorithm = methods[args.method]
+        with cam_algorithm(model=model,
+                           target_layers=target_layers,
+                           use_cuda=args.use_cuda) as cam:
 
-        # AblationCAM and ScoreCAM have batched implementations.
-        # You can override the internal batch size for faster computation.
-        cam.batch_size = 32
+            # AblationCAM and ScoreCAM have batched implementations.
+            # You can override the internal batch size for faster computation.
+            cam.batch_size = 32
 
-        grayscale_cam = cam(input_tensor=input_tensor,
-                            target_category=target_category,
-                            aug_smooth=args.aug_smooth,
-                            eigen_smooth=args.eigen_smooth)
+            grayscale_cam = cam(input_tensor=input_tensor,
+                                target_category=target_category,
+                                aug_smooth=args.aug_smooth,
+                                eigen_smooth=args.eigen_smooth)
 
-        # Here grayscale_cam has only one image in the batch
-        grayscale_cam = grayscale_cam[0, :]
+            # Here grayscale_cam has only one image in the batch
+            grayscale_cam = grayscale_cam[0, :]
 
-        cam_image = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+            cam_image = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
 
-        # cam_image is RGB encoded whereas "cv2.imwrite" requires BGR encoding.
-        cam_image = cv2.cvtColor(cam_image, cv2.COLOR_RGB2BGR)
+            # cam_image is RGB encoded whereas "cv2.imwrite" requires BGR encoding.
+            cam_image = cv2.cvtColor(cam_image, cv2.COLOR_RGB2BGR)
 
-    gb_model = GuidedBackpropReLUModel(model=model, use_cuda=args.use_cuda)
-    gb = gb_model(input_tensor, target_category=target_category)
+        gb_model = GuidedBackpropReLUModel(model=model, use_cuda=args.use_cuda)
+        gb = gb_model(input_tensor, target_category=target_category)
 
-    cam_mask = cv2.merge([grayscale_cam, grayscale_cam, grayscale_cam])
-    cam_gb = deprocess_image(cam_mask * gb)
-    gb = deprocess_image(gb)
+        cam_mask = cv2.merge([grayscale_cam, grayscale_cam, grayscale_cam])
+        cam_gb = deprocess_image(cam_mask * gb)
+        gb = deprocess_image(gb)
 
-    cv2.imwrite(f'{args.method}_cam.jpg', cam_image)
-    cv2.imwrite(f'{args.method}_gb.jpg', gb)
-    cv2.imwrite(f'{args.method}_cam_gb.jpg', cam_gb)
+        # f'的作用是将字符串格式化 可以直接读取 变量 的值
+        # 注意一点 若原图为png 存储热力图为jpg 可以避免多次实验中 将上次实验的 热力图 也作为 输入
+        filename = os.path.basename(image)
+        # 此处split的用法为 以 . 为分隔符 取分隔后的第一个字符串
+        cv2.imwrite('./insects/' + filename.split('.')[0] + f'_{args.method}_cam.jpg', cam_image)
+        cv2.imwrite('./insects/' + filename.split('.')[0] + f'_{args.method}_gb.jpg', gb)
+        cv2.imwrite('./insects/' + filename.split('.')[0] + f'_{args.method}_cam_gb.jpg', cam_gb)
